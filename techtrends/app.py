f372c758213b89fb1284423b7007ff0cb0ec2ff7
Iñigo Iparragirre <inigoiparraguirre@gmail.com>
2021-10-06 09:30:46 +0200

    Add date to logs

--- techtrends/app.py
+++ techtrends/app.py
@@ -119,5 +119,5 @@ def metrics():
 
 # start the application on port 3111
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
     app.run(host='0.0.0.0', port='3111')
